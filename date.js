2b70ac2bc69da83b49d6c29228faad8312576244
Daniyaal S Nayeem <daniyaalnayeem78653@outlook.com>
2022-08-09 00:21:32 +0530

    Added Dynamically Rendered Year

--- date.js
+++ date.js
@@ -25,3 +25,10 @@ exports.getDay = function () {
   return today.toLocaleDateString("en-US", options);
 
 };
+
+exports.getYear = function () {
+
+  const today = new Date();
+  return today.getFullYear();
+
+};
